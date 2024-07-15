337224c8bb4731fb780094fc9c66c6cc78e2da57
zion <ajz317@nyu.edu>
2024-07-15 12:32:15 +0600

    updated home page

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
+
 const Home = () => {
-    return ( 
-        <div className="home">
-            <h2>HomePage</h2>
-        </div>
-     );
-}
- 
+  useEffect(() => {
+    try {
+      console.log("Home Page"); 
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+  return (
+    <div className="home">
+      <h2>𝓗𝓸𝓶𝓮 𝓟𝓪𝓰𝓮</h2>
+      <img src="http://localhost:3000/IMG_2413.JPG" alt="AUTISM" />
+    </div>
+  );
+};
+
 export default Home;
